ababfd900c7db63e09c1e24bd3693e972d56889b
Rohith S <rohi.devs@gmail.com>
2025-02-19 00:41:50 +0530

    swagger api

---
+++ swagger.js
@@ -0,0 +1,48 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'CTF Events API',
+      version: '1.0.0',
+      description: 'API documentation for CTF Events application',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000',
+        description: 'Development server',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    tags: [
+      {
+        name: 'Admin',
+        description: 'Admin authentication endpoints',
+      },
+      {
+        name: 'Users',
+        description: 'User authentication endpoints',
+      },
+      {
+        name: 'Events',
+        description: 'Event management endpoints',
+      },
+      {
+        name: 'Announcements',
+        description: 'Announcement management endpoints',
+      },
+    ],
+  },
+  apis: ['./final.js'], // Path to the API docs
+};
+
+module.exports = swaggerJsdoc(options);
