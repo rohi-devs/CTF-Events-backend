e7737d9927f3f587b85f0ff1d6a70074f73f1304
Rohith S <rohi.devs@gmail.com>
2025-02-19 00:49:37 +0530

    swagger api

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000',
+        url: 'https://event.ctf.rohidevs.engineer',
         description: 'Development server',
       },
     ],
